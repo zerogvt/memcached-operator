6cb3690b43aa1451b9bec5f468d581554cdba0a9
vas <vasigkou@ie.ibm.com>
2020-01-24 13:52:27 +0200

    first commit

--- pkg/apis/cache/v1alpha1/memcached_types.go
+++ pkg/apis/cache/v1alpha1/memcached_types.go
@@ -7,44 +7,42 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// MemcahcedSpec defines the desired state of Memcahced
-type MemcahcedSpec struct {
+// MemcachedSpec defines the desired state of Memcached
+type MemcachedSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	Size int32 `json:"size"`
 }
 
-// MemcahcedStatus defines the observed state of Memcahced
-type MemcahcedStatus struct {
+// MemcachedStatus defines the observed state of Memcached
+type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	Nodes []string `json:"nodes"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Memcahced is the Schema for the memcahceds API
+// Memcached is the Schema for the memcacheds API
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:path=memcahceds,scope=Namespaced
-type Memcahced struct {
+// +kubebuilder:resource:path=memcacheds,scope=Namespaced
+type Memcached struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   MemcahcedSpec   `json:"spec,omitempty"`
-	Status MemcahcedStatus `json:"status,omitempty"`
+	Spec   MemcachedSpec   `json:"spec,omitempty"`
+	Status MemcachedStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// MemcahcedList contains a list of Memcahced
-type MemcahcedList struct {
+// MemcachedList contains a list of Memcached
+type MemcachedList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Memcahced `json:"items"`
+	Items           []Memcached `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&Memcahced{}, &MemcahcedList{})
+	SchemeBuilder.Register(&Memcached{}, &MemcachedList{})
 }
